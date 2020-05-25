6328681e38bf43106e8878e6a9084dc989e12cd7
PRANEET <praneet.rane@gmail.com>
2020-05-25 08:40:42 +0530

    initial Commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        List<Customer> customers = new List<Customer>
+            {
+
+            };
+        // GET: /Customers/
+        [Route("Customers/CustomerList")]
+        public ActionResult CustomerList()
+        {
+
+            var customerList = GetAllCustomers();
+            
+            return View(customerList);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Customer customer = GetAllCustomers().Where(x => x.Id == id).FirstOrDefault();
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetAllCustomers()
+        {
+            try
+            {
+                return new List<Customer>{
+                    new Customer{Id=1, Name="Mike"},
+                    new Customer{Id=2,Name="Jonathan"}
+                };
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error while getting customer details: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //
+        // GET: /Movies/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { ID = 1, Name = "Shrek!" };
+
+            var customers = new List<Customer>{
+                new Customer{Id=1, Name ="Alex"},
+                new Customer{Id=2, Name="Matt"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+     
+
+        [Route("Movies/ByReleasedDate/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+
+        public ContentResult MovieByRealsedDate(int month, int year)
+        {
+            return Content(string.Format("Month {0} , Year{1}", month, year));
+        }
+
+        public ViewResult MovieList()
+        {
+
+            var movieList = GetAllMovies();
+
+            return View(movieList);
+        }
+
+        public ViewResult Details(int id)
+        {
+            var movie = GetAllMovies().Where(x => x.ID == id).FirstOrDefault();
+
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetAllMovies()
+        {
+            try
+            {
+                return new List<Movie>{
+                    new Movie{ID=1,Name="Shrek!"},
+                    new Movie{ID=2,Name="Wall E"}
+                };
+            }
+            catch (Exception ex)
+            {
+                
+                throw new Exception("Error while getting all movies: " +ex.Message);
+            }
+        }
+
+    }
+}
