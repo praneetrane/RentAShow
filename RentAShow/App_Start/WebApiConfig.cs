c84cd25ec1abe60b82c7d65dadefe762319c95b3
PRANEET <praneet.rane@gmail.com>
2020-06-16 07:40:12 +0530

    69 | Camel Notation

--- RentAShow/App_Start/WebApiConfig.cs
+++ RentAShow/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -9,6 +10,10 @@ namespace RentAShow
     {
         public static void Register(HttpConfiguration config)
         {
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Newtonsoft.Json.Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
